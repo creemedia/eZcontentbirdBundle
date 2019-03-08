007de339a9774bf6256e6bbf472aaecada46137a
Bekir Öztürk <bo@creemedia.com>
2019-03-08 11:43:28 +0100

    refactored code + added image text parsing

--- Common/CmsService.php
+++ Common/CmsService.php
@@ -10,177 +10,226 @@ use eZ\Publish\API\Repository\Values\Content\Query\Criterion;
 
 class CmsService
 {
-	private $container;
-	private $repository;
-	private $contentService;
-	private $locationService;
-	private $contentTypeService;
-	private $userService;
-	private $searchService;
+    private $container;
+    private $repository;
+    private $contentService;
+    private $locationService;
+    private $contentTypeService;
+    private $userService;
+    private $searchService;
 
-	protected $allowedAdditionFields = ['summary', 'image'];
+    protected $allowedAdditionFields = ['summary', 'image'];
 
-	const IMAGE_LOCATION_TO_UPLOAD = '274569';
-	const RATGEBER_LOCATION_ID = 257441;
+    const IMAGE_LOCATION_TO_UPLOAD = '274569';
+    const RATGEBER_LOCATION_ID = 257441;
 
-	public function __construct(Container $container, Repository $repository)
-	{
-		$this->container = $container;
-		$this->repository = $repository;
-		$this->contentService = $this->repository->getContentService();
-		$this->locationService = $this->repository->getLocationService();
-		$this->contentTypeService = $this->repository->getContentTypeService();
-		$this->userService = $this->repository->getUserService();
-		$this->searchService = $this->repository->getSearchService();
-	}
+    public function __construct(Container $container, Repository $repository)
+    {
+        $this->container = $container;
+        $this->repository = $repository;
+        $this->contentService = $this->repository->getContentService();
+        $this->locationService = $this->repository->getLocationService();
+        $this->contentTypeService = $this->repository->getContentTypeService();
+        $this->userService = $this->repository->getUserService();
+        $this->searchService = $this->repository->getSearchService();
+    }
 
-	public function createContent($parentLocation, $contentType, $title, $text, $post_status, $userId, $keywords, $additionalFields)
-	{
-		$user = $this->userService->loadUser($userId);
-		$this->repository->setCurrentUser($user);
+    /**
+     * @param $parentLocation
+     * @param $contentType
+     * @param $title
+     * @param $text
+     * @param $post_status
+     * @param $userId
+     * @param $keywords
+     * @param $additionalFields
+     * @return \eZ\Publish\API\Repository\Values\Content\Content
+     * @throws \eZ\Publish\API\Repository\Exceptions\ContentFieldValidationException
+     * @throws \eZ\Publish\API\Repository\Exceptions\ContentValidationException
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function createContent($parentLocation, $contentType, $title, $text, $post_status, $userId, $keywords, $additionalFields)
+    {
+        $user = $this->userService->loadUser($userId);
+        $this->repository->setCurrentUser($user);
 
-		$contentType = $this->contentTypeService->loadContentTypeByIdentifier($contentType);
-		$contentCreateStruct = $this->contentService->newContentCreateStruct($contentType, 'eng-GB');
+        $contentType = $this->contentTypeService->loadContentTypeByIdentifier($contentType);
+        $contentCreateStruct = $this->contentService->newContentCreateStruct($contentType, 'eng-GB');
 
-		if (!empty($keywords)) {
-			$contentCreateStruct->setField('cb_keywords', implode(';', $keywords));
-		}
+        if (!empty($keywords)) {
+            $contentCreateStruct->setField('cb_keywords', implode(';', $keywords));
+        }
 
-		$contentCreateStruct->setField('title', $title);
-		$contentCreateStruct->setField('text', '<section xmlns="http://ez.no/namespaces/ezpublish5/xhtml5/edit">' . $text . '</section>');
+        $contentCreateStruct->setField('title', $title);
+        $contentCreateStruct->setField('text', '<section xmlns="http://ez.no/namespaces/ezpublish5/xhtml5/edit">' . $text . '</section>');
 
-		$contentCreateStruct = $this->handleAdditionalFields($additionalFields, $contentCreateStruct);
+        $contentCreateStruct = $this->handleAdditionalFields($additionalFields, $contentCreateStruct);
 
-		$locationCreateStruct = $this->locationService->newLocationCreateStruct($parentLocation[0]);
+        $locationCreateStruct = $this->locationService->newLocationCreateStruct($parentLocation[0]);
 
-		$draft = $this->contentService->createContent($contentCreateStruct, array($locationCreateStruct));
+        $draft = $this->contentService->createContent($contentCreateStruct, array($locationCreateStruct));
 
-		return $draft;
-	}
+        return $draft;
+    }
 
-	private function handleAdditionalFields($fields, $contentCreateStruct)
-	{
-		foreach ($fields as $field) {
-			if (in_array($field['name'], $this->allowedAdditionFields)) {
-				if ($field['name'] === 'summary') { // BEI SUMMARY MUSS es in p liegen
-					$contentCreateStruct->setField($field['name'], '<section xmlns="http://ez.no/namespaces/ezpublish5/xhtml5/edit"><p>' . $field['content'] . '</p></section>');
-				} else if (isset($field['content'])) {
-					$contentCreateStruct->setField($field['name'], $field['content']);
-				}
-			}
-		}
-		return $contentCreateStruct;
-	}
+    private function handleAdditionalFields($fields, $contentCreateStruct)
+    {
+        foreach ($fields as $field) {
+            if (in_array($field['name'], $this->allowedAdditionFields)) {
+                if ($field['name'] === 'summary') { // BEI SUMMARY MUSS es in p liegen
+                    $contentCreateStruct->setField($field['name'], '<section xmlns="http://ez.no/namespaces/ezpublish5/xhtml5/edit"><p>' . $field['content'] . '</p></section>');
+                } else if (isset($field['content'])) {
+                    $contentCreateStruct->setField($field['name'], $field['content']);
+                }
+            }
+        }
+        return $contentCreateStruct;
+    }
 
-	public function updateContent($contentId, $newBody, $newTitle, $keywords, $additionalFields)
-	{
-		$contentInfo = $this->contentService->loadContentInfo($contentId);
-		$userId = $this->userService->loadUser($contentInfo->ownerId);
-		$this->repository->setCurrentUser($userId);
+    /**
+     * @param $contentId
+     * @param $newBody
+     * @param $newTitle
+     * @param $keywords
+     * @param $additionalFields
+     * @return \eZ\Publish\API\Repository\Values\Content\Content
+     * @throws \eZ\Publish\API\Repository\Exceptions\BadStateException
+     * @throws \eZ\Publish\API\Repository\Exceptions\ContentFieldValidationException
+     * @throws \eZ\Publish\API\Repository\Exceptions\ContentValidationException
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function updateContent($contentId, $newBody, $newTitle, $keywords, $additionalFields)
+    {
+        $contentInfo = $this->contentService->loadContentInfo($contentId);
+        $userId = $this->userService->loadUser($contentInfo->ownerId);
+        $this->repository->setCurrentUser($userId);
 
-		if ($contentInfo->isPublished()) {
-			$contentDraft = $this->contentService->createContentDraft($contentInfo);
-		} else if ($contentInfo->isDraft()) {
-			$versionInfo = $this->contentService->loadVersionInfoById($contentId);
-		}
+        if ($contentInfo->isPublished()) {
+            $contentDraft = $this->contentService->createContentDraft($contentInfo);
+        } else if ($contentInfo->isDraft()) {
+            $versionInfo = $this->contentService->loadVersionInfoById($contentId);
+        }
 
-		$contentUpdateStruct = $this->contentService->newContentUpdateStruct();
-		$contentUpdateStruct->initialLanguageCode = 'eng-GB';
+        $contentUpdateStruct = $this->contentService->newContentUpdateStruct();
+        $contentUpdateStruct->initialLanguageCode = 'eng-GB';
 
-		if ($newTitle) {
-			$contentUpdateStruct->setField('title', $newTitle);
-		}
+        if ($newTitle) {
+            $contentUpdateStruct->setField('title', $newTitle);
+        }
 
-		if (!empty($keywords)) {
-			$contentUpdateStruct->setField('cb_keywords', implode(';', $keywords));
-		} else {
-			$contentUpdateStruct->setField('cb_keywords', '');
-		}
+        if (!empty($keywords)) {
+            $contentUpdateStruct->setField('cb_keywords', implode(';', $keywords));
+        } else {
+            $contentUpdateStruct->setField('cb_keywords', '');
+        }
 
-		$contentUpdateStruct->setField('text', '<section xmlns="http://ez.no/namespaces/ezpublish5/xhtml5/edit">' . $newBody . '</section>');
-		$contentUpdateStruct = $this->handleAdditionalFields($additionalFields, $contentUpdateStruct);
+        $contentUpdateStruct->setField('text', '<section xmlns="http://ez.no/namespaces/ezpublish5/xhtml5/edit">' . $newBody . '</section>');
+        $contentUpdateStruct = $this->handleAdditionalFields($additionalFields, $contentUpdateStruct);
 
-		if ($contentInfo->isPublished()) {
-			$contentDraft = $this->contentService->updateContent($contentDraft->versionInfo, $contentUpdateStruct);
-		} else if ($contentInfo->isDraft()) {
-			$contentDraft = $this->contentService->updateContent($versionInfo, $contentUpdateStruct);
-		}
-		return $contentDraft;
-	}
+        if ($contentInfo->isPublished()) {
+            $contentDraft = $this->contentService->updateContent($contentDraft->versionInfo, $contentUpdateStruct);
+        } else if ($contentInfo->isDraft()) {
+            $contentDraft = $this->contentService->updateContent($versionInfo, $contentUpdateStruct);
+        }
+        return $contentDraft;
+    }
 
-	public function uploadImage($imageName, $fileName, $alternativeText)
-	{
-		$this->repository->setCurrentUser($this->userService->loadUser(14));
-		$contentType = $this->contentTypeService->loadContentTypeByIdentifier("image");
-		$contentCreateStruct = $this->contentService->newContentCreateStruct($contentType, 'eng-GB');
+    /**
+     * @param $fileName
+     * @param $caption
+     * @param $value
+     * @return int|mixed
+     * @throws \eZ\Publish\API\Repository\Exceptions\BadStateException
+     * @throws \eZ\Publish\API\Repository\Exceptions\ContentFieldValidationException
+     * @throws \eZ\Publish\API\Repository\Exceptions\ContentValidationException
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function uploadImage($fileName, $caption, $value)
+    {
+        $this->repository->setCurrentUser($this->userService->loadUser(14));
+        $contentType = $this->contentTypeService->loadContentTypeByIdentifier("image");
+        $contentCreateStruct = $this->contentService->newContentCreateStruct($contentType, 'eng-GB');
 
-		$contentCreateStruct->setField('name', $fileName);
+        $contentCreateStruct->setField('name', $fileName);
+        $contentCreateStruct->setField('caption', '<section xmlns="http://ez.no/namespaces/ezpublish5/xhtml5/edit"><p>' . $caption . '</p></section>');
+        $contentCreateStruct->setField('image', $value);
 
-		$value = new Value([
-			'path' => $imageName,
-			'fileSize' => filesize($imageName),
-			'fileName' => $fileName,
-			'alternativeText' => $alternativeText,
-		]);
+        $draft = $this->contentService->createContent($contentCreateStruct,
+            [$this->locationService->newLocationCreateStruct(self::IMAGE_LOCATION_TO_UPLOAD)]
+        );
 
-		$contentCreateStruct->setField('image', $value);
+        return $this->contentService->publishVersion($draft->versionInfo)->id ?? -1;
+    }
 
-		$draft = $this->contentService->createContent($contentCreateStruct,
-			[$this->locationService->newLocationCreateStruct(self::IMAGE_LOCATION_TO_UPLOAD)]
-		);
+    /**
+     * @return array
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     * @throws \eZ\Publish\API\Repository\Exceptions\UnauthorizedException
+     */
+    public function getAllCategories()
+    {
+        $ratgeberMainLocation = $this->locationService->loadLocation(self::RATGEBER_LOCATION_ID);
 
-		return $this->contentService->publishVersion($draft->versionInfo)->id ?? -1;
-	}
+        $query = new Query();
+        $query->query = new Criterion\LogicalOr([
+            new Criterion\LogicalAnd([
+                new Criterion\Subtree($ratgeberMainLocation->pathString),
+                new Criterion\Visibility(Criterion\Visibility::VISIBLE),
+                new Criterion\ContentTypeIdentifier(['overview', 'dossier'])
+            ])
+        ]);
 
-	public function getAllCategories()
-	{
-		$ratgeberMainLocation = $this->locationService->loadLocation(self::RATGEBER_LOCATION_ID);
+        $query->limit = 2000; // :)
 
-		$query = new Query();
-		$query->query = new Criterion\LogicalOr([
-			new Criterion\LogicalAnd([
-				new Criterion\Subtree($ratgeberMainLocation->pathString),
-				new Criterion\Visibility(Criterion\Visibility::VISIBLE),
-				new Criterion\ContentTypeIdentifier(['overview', 'dossier'])
-			])
-		]);
+        $data = array_map(function ($hit) {
+            return $hit->valueObject;
+        }, $this->searchService->findContent($query)->searchHits);
+        return $data;
+    }
 
-		$query->limit = 2000; // :)
+    /**
+     * @return array
+     * @throws \eZ\Publish\API\Repository\Exceptions\NotFoundException
+     */
+    public function getContentTypes()
+    {
+        $contentTypeService = $this->repository->getContentTypeService();
+        $contentGroup = $contentTypeService->loadContentTypeGroup(1);
+        $contentTypes = $contentTypeService->loadContentTypes($contentGroup);
 
-		$data = array_map(function ($hit) {
-			return $hit->valueObject;
-		}, $this->searchService->findContent($query)->searchHits);
-		return $data;
-	}
+        $types = [];
 
-	public function getContentTypes()
-	{
-		$contentTypeService = $this->repository->getContentTypeService();
-		$contentGroup = $contentTypeService->loadContentTypeGroup(1);
-		$contentTypes = $contentTypeService->loadContentTypes($contentGroup);
+        for ($i = 0; $i < count($contentTypes); $i++) {
+            $typ = $contentTypes[$i]->identifier;
+            $typ = ['name' => $typ, 'label' => $typ];
 
-		$types = [];
+            array_push($types, $typ);
+        }
 
-		for ($i = 0; $i < count($contentTypes); $i++) {
-			$typ = $contentTypes[$i]->identifier;
-			$typ = ['name' => $typ, 'label' => $typ];
+        return $types;
+    }
 
-			array_push($types, $typ);
-		}
+    /**
+     * @return array
+     * @throws \eZ\Publish\API\Repository\Exceptions\InvalidArgumentException
+     */
+    public function getUsers()
+    {
+        $query = new Query();
+        $query->filter = new Criterion\LogicalAnd([
+            new Criterion\ContentId([172776, 172749, 202610])
+        ]);
 
-		return $types;
-	}
-
-	public function getUsers()
-	{
-		$query = new Query();
-		$query->filter = new Criterion\LogicalAnd([
-			new Criterion\ContentId([172776, 172749, 202610])
-		]);
-
-		$usersContent = array_map(function ($hit) {
-			return $hit->valueObject;
-		}, $this->repository->getSearchService()->findContent($query)->searchHits);
-		return $usersContent;
-	}
+        $usersContent = array_map(function ($hit) {
+            return $hit->valueObject;
+        }, $this->repository->getSearchService()->findContent($query)->searchHits);
+        return $usersContent;
+    }
 }
--- Helper/ImageHelper.php
+++ Helper/ImageHelper.php
@@ -8,81 +8,105 @@ use CM\ExtendedImageBundle\eZ\Publish\FieldType\ExtendedImage\Value;
 
 class ImageHelper
 {
-	/**
-	 * @param $src
-	 * @param $path
-	 */
-	public function downloadImageLocal($src, $path)
-	{
-		$image = file_get_contents($src);
-		$fp = fopen($path, "w");
-		fwrite($fp, $image);
-		fclose($fp);
-	}
+    const INDEX_IMAGE_DESCRIPTION = 0;
+    const INDEX_IMAGE_ALT = 1;
+    const INDEX_IMAGE_CREATOR = 2;
+    const INDEX_IMAGE_COPYRIGHT_HOLDER = 3;
+    const INDEX_IMAGE_COPYRIGHT_YEAR = 4;
 
-	/**
-	 * @param $nodeDiv
-	 */
-	public function handleImageFallbacks(&$nodeDiv)
-	{
-		if ($nodeDiv->parentNode->tagName === 'strong' && !empty($nodeDiv->parentNode->parentNode->tagName)) {
-			$nodeDiv->parentNode->parentNode->replaceChild($nodeDiv, $nodeDiv->parentNode);
-		}
+    /**
+     * @param $src
+     * @param $path
+     */
+    public function downloadImageLocal($src, $path)
+    {
+        $image = file_get_contents($src);
+        $fp = fopen($path, "w");
+        fwrite($fp, $image);
+        fclose($fp);
+    }
 
-		if ($nodeDiv->parentNode->tagName === 'p' && !empty($nodeDiv->parentNode->parentNode->tagName)) {
-			$nodeDiv->parentNode->parentNode->replaceChild($nodeDiv, $nodeDiv->parentNode);
-		}
-	}
+    /**
+     * @param $nodeDiv
+     */
+    public function handleImageFallbacks(&$nodeDiv)
+    {
+        if ($nodeDiv->parentNode->tagName === 'strong' && !empty($nodeDiv->parentNode->parentNode->tagName)) {
+            $nodeDiv->parentNode->parentNode->replaceChild($nodeDiv, $nodeDiv->parentNode);
+        }
 
-	/**
-	 * @param $contentId
-	 * @param $doc
-	 * @param $tag
-	 * @return node
-	 */
-	public function createEzEmbed($contentId, $doc, &$tag)
-	{
-		$ezEmbed = '<div data-ezelement="ezembed" data-href="ezcontent://' . $contentId . '" data-ezview="embed"/>';
-		$nodeDiv = $doc->createTextNode($ezEmbed);
-		$tag->parentNode->replaceChild($nodeDiv, $tag);
-		return $nodeDiv;
-	}
+        if ($nodeDiv->parentNode->tagName === 'p' && !empty($nodeDiv->parentNode->parentNode->tagName)) {
+            $nodeDiv->parentNode->parentNode->replaceChild($nodeDiv, $nodeDiv->parentNode);
+        }
+    }
 
-	/**
-	 * @param $imagePath
-	 * @param $shortCodes
-	 * @throws \eZ\Publish\Core\Base\Exceptions\InvalidArgumentType
-	 */
-	public function handleCoverImageFromShortCodes($imagePath, &$shortCodes)
-	{
-		foreach ($shortCodes as &$code) {
-			if ($code['name'] === 'image' && isset($code['content'])) {
-				$xpath = new DOMXPath(@DOMDocument::loadHTML($code['content']));
-				$src = $xpath->evaluate("string(//img/@src)");
-				$alt = $xpath->evaluate("string(//img/@alt)");
+    /**
+     * @param $contentId
+     * @param $doc
+     * @param $tag
+     * @return node
+     */
+    public function createEzEmbed($contentId, $doc, &$tag)
+    {
+        $ezEmbed = '<div data-ezelement="ezembed" data-href="ezcontent://' . $contentId . '" data-ezview="embed"/>';
+        $nodeDiv = $doc->createTextNode($ezEmbed);
+        $tag->parentNode->replaceChild($nodeDiv, $tag);
+        return $nodeDiv;
+    }
 
-				$this->downloadImageLocal($src, $imagePath);
+    /**
+     * @param $imagePath
+     * @param $shortCodes
+     * @throws \eZ\Publish\Core\Base\Exceptions\InvalidArgumentType
+     */
+    public function handleCoverImageFromShortCodes($imagePath, &$shortCodes)
+    {
+        print_r($shortCodes);
+        foreach ($shortCodes as &$code) {
+            if ($code['name'] === 'image' && isset($code['content'])) {
+                $xpath = new DOMXPath(@DOMDocument::loadHTML($code['content']));
+                $src = $xpath->evaluate("string(//img/@src)");
+                $alt = $xpath->evaluate("string(//img/@alt)");
 
-				$value = $this->generateValue($src, $alt, $imagePath);
-				$code['content'] = $value;
-			}
-		}
-	}
+                $this->downloadImageLocal($src, $imagePath);
+                $imageMeta = $this->parseImageAlt($alt);
 
-	/**
-	 * @param $src
-	 * @param $alt
-	 * @param $path
-	 * @return Value
-	 * @throws \eZ\Publish\Core\Base\Exceptions\InvalidArgumentType
-	 */
-	public function generateValue($src, $alt, $path)
-	{
-		$value = new Value([
-			'fileName' => basename($src),
-			'path' => $path,
-			'alternativeText' => $alt
-		]);
-		return $value;
-	}
+                $value = $this->generateValue($src,$imagePath, $imageMeta);
+                $code['content'] = $value;
+            }
+        }
+    }
+
+    /**
+     * @param $src
+     * @param $path
+     * @param $imageMeta
+     * @return Value
+     * @throws \eZ\Publish\Core\Base\Exceptions\InvalidArgumentType
+     */
+    public function generateValue($src, $path, $imageMeta)
+    {
+        $value = new Value([
+            'fileName' => basename($src),
+            'name' => basename($src),
+            'path' => $path,
+            'alternativeText' => $imageMeta[self::INDEX_IMAGE_ALT] ?? '',
+            'creator' => $imageMeta[self::INDEX_IMAGE_CREATOR] ?? '',
+            'copyrightHolder' => $imageMeta[self::INDEX_IMAGE_COPYRIGHT_HOLDER] ?? '',
+            'copyrightYear' => $imageMeta[self::INDEX_IMAGE_COPYRIGHT_YEAR] ?? '',
+            'fileSize' => filesize($path)
+        ]);
+        return $value;
+    }
+
+    /**
+     * @param $alt
+     * @return array
+     */
+    public function parseImageAlt($alt)
+    {
+        $splitted = explode(';', $alt);
+        // fallbacks later...
+        return $splitted;
+    }
 }
